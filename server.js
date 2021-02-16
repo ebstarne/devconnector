b1bc6adb2b0b0087435e32da88ec1d1889ea2fc8
Eric <estarner1229@gmail.com>
2021-02-16 04:48:52 -0500

    Final changes for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,11 +8,11 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-    extended: false
-}));
-
-app.get('/', (req, res) => res.send('API Running'));
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -19,6 +20,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
